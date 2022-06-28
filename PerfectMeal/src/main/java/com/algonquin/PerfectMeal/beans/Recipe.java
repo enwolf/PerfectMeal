7a178b2316c37b2392d8742d5d3e0107d35363f8
teepidge <taylor.pigeon@gmail.com>
2022-06-27 23:42:13 -0400

    Lots of boilerplate code
    Changed HTML landing page. Will need to fix <span> elements.  Added form
    wrappers for actions
    
    Added lots of boilerplate code to start the ball rolling.
    
    Added DAO, Recipe/RecipeCard beans with minor inputs
    
    Created a "resources" package to house the DDL for the SQL DB. Don't
    know if this will work or not, but thought it would be worth the try!
    
    Added servlets for create user and login.  no functionality added.
    Probably should wait and add working functionality before adjusting ...
    :(  Thinking I need to focus on one aspect of the application, and I
    think that's getting the DB up and running easily/efficiently.  Probably
    need to use the init() function in servlet lifecycle to enable it and
    fill it with data.

---
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/Recipe.java
@@ -0,0 +1,10 @@
+package com.algonquin.PerfectMeal.beans;
+
+import java.util.UUID;
+
+public class Recipe {
+	private UUID id;
+	private String name;
+	private String category;
+
+}
---
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/dao/DBConnection.java
@@ -0,0 +1,58 @@
+package com.algonquin.PerfectMeal.dao;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnection {
+
+	private static final String DBUSER = "root";
+	private static final String DBPASSWORD = "";
+	private static final String CONN_STRING = "jdbc:mysql://localhost:3306/PerfectMeal";
+	private static final String DB_INITIALIZER_FILEPATH = "com.algonquin.PerfectMeal.resources/DDL.sql";
+
+	public static void CreateDatabase() throws IOException {
+		// TODO: determine relative file path of the SQL file to call this method
+		// from a servlet
+
+		// create a file path
+		Path fileName = Path.of(DB_INITIALIZER_FILEPATH);
+
+		// create the query string
+		String createDBQuery = Files.readString(fileName);
+
+		try {
+
+			Connection connection = DBConnection.getConnectionToDatabase();
+
+			String insertQuery = "insert into logs (uuid, title, content, createTimestamp)  values (?, ?, ?, ?)";
+
+			Statement statement = connection.createStatement();
+			statement.execute(createDBQuery);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static Connection getConnectionToDatabase() throws SQLException {
+		Connection connection = null;
+//		Class.forName("com.mysql.jdbc.Driver");
+
+		try {
+			connection = DriverManager.getConnection(CONN_STRING, DBUSER, DBPASSWORD);
+			System.out.println("Connected");
+		} catch (SQLException e) {
+			System.out.println("blah");
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+
+}
