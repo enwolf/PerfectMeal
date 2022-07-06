c1ec3174d109642a49db7972351bc723f371cc86
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-06 00:34:47 -0400

    After looking at the try catch and error printing in RegisteredUserServlet.java I changed my first approach as this might offer a more elegant solution.
    I'm duplicating using one large try catch statement and if statements
    that throw new exceptions to be updated on the error.jsp page
    
    This also I believe has the benefited of no longer requiring an if
    (condition && condition) check as if no exception is thrown then both
    conditions should be true
    
    My original code is currently included but commented out for now.

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/servlets/LoginServlet.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/servlets/LoginServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.algonquin.PerfectMeal.dao.UserDAO;
+
 public class LoginServlet extends HttpServlet {
 
 	@Override
@@ -19,21 +21,63 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		
-		
+		//Strings to store values entered by user for login
 		String loginEmail = request.getParameter("email");
 		String password = request.getParameter("password");
+
+		//Booleans to check if users exists and are validated. 
+		boolean userExists = false;
+		boolean userValidated = false;
+		
+		
+		UserDAO userDAO = new UserDAO();
+		/*
+		try {
+			userExists = userDAO.userExists(loginEmail);
+		} catch (ClassNotFoundException e) {
+			System.out.println("User not found");
+			e.printStackTrace();
+		}
+		
+		try {
+			userValidated = userDAO.isValidatedByEmail(loginEmail);
+		} catch (ClassNotFoundException e) {
+			System.out.println("User account not verified.  ");
+			e.printStackTrace();
+		}
+		*/
+		
+		try {
+			if(!userDAO.userExists(loginEmail))
+				throw new Exception("User not found");
+			
+			if(!userDAO.isValidatedByEmail(loginEmail))
+				throw new Exception("User account not verified.");
+				
+		}catch (Exception e) {
+
+			// forward to error page for debugging
+			request.setAttribute("errorMessage", e.getMessage());
+			RequestDispatcher errorDispatcher = request.getRequestDispatcher("/html/error.jsp");
+			errorDispatcher.forward(request, response);
+		}
+		
+		/*
+		if (userExists == true && userValidated == true)
+			System.out.println("user exists and has validated account");
 		
 		
 		
-		
+		//sys.out messages for testing. 
 		System.out.println("Pressed login!!");
 		System.out.println("Email input value = " + loginEmail);
 		System.out.println("Password input value = " + password);
+		System.out.println("userExists = " + userExists);
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/html/login.jsp");
 		dispatcher.forward(request, response);
 		
-		
+		*/
 		
 	}
 
