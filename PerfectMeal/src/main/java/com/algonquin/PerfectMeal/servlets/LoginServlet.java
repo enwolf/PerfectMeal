74c87d889ab7b7a0d0f55a2586213c5201fbcbae
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-05 21:52:49 -0400

    Updated LoginServlet.java
     - added simple sys.out message and dispatcher to redirect to login.jsp
    
    currently working correctly.

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/servlets/LoginServlet.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/servlets/LoginServlet.java
@@ -2,6 +2,7 @@ package com.algonquin.PerfectMeal.servlets;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,18 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		
+		
+		
+		
+		
+		
+		System.out.println("Pressed login!!");
+		
+		
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/html/login.jsp");
+		dispatcher.forward(req, resp);
 	}
 
 }
