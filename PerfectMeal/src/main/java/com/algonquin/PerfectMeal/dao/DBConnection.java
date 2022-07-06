f65a43e1f1c43488df7da503da5fffe21a6d56c8
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-05 22:21:58 -0400

    Updated DBPASSWORD to my local MySQL password.

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/dao/DBConnection.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/dao/DBConnection.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 public class DBConnection {
 
 	private static final String DBUSER = "root";
-	private static final String DBPASSWORD = "";
+	private static final String DBPASSWORD = "password";
 	private static final String CONN_STRING = "jdbc:mysql://localhost:3306/PerfectMeal";
 	private static final String DB_INITIALIZER_FILEPATH = "com.algonquin.PerfectMeal.resources/DDL.sql";
 
