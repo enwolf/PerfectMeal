4676ab005944dcbe874bab7f40eae15b3be51737
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-08 04:57:23 -0400

    Updated Servlet UpdateUseProfileServlet.java
    After fixing my error in login.jsp I was able to uncomment and remove my
    testing code and implement my original method of updating the user.
    
    I am able to pull new vales from the <input> fields on login.jsp
    pass them to the UserDAO.updateUserDataInDatabase() method and and the
    corresponding string values will be updated in the database.
    
    An update success message is also displayed and the userProfile.jsp
    page is reloaded.
    
    My submission for Capstone2 is now feature complete!!!!
    
    ^_^    ^_^    ^_^

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/servlets/UpdateUserProfileServlet.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/servlets/UpdateUserProfileServlet.java
@@ -3,8 +3,8 @@ package com.algonquin.PerfectMeal.servlets;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,99 +13,61 @@ import javax.servlet.http.HttpSession;
 import com.algonquin.PerfectMeal.beans.User;
 import com.algonquin.PerfectMeal.dao.UserDAO;
 
-
 public class UpdateUserProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		/*
-		String newFirstName = request.getParameter("firstName");
-		String newLastName = request.getParameter("LastName");
-		String newPassword = request.getParameter("Password");
-		
-		
-		
-		
-		System.out.println(newFirstName);
-		System.out.println(newLastName);
-		System.out.println(newPassword);
-		*/
-		
-		
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		/*
-		String firstName = request.getParameter("firstName");
-		String lastName = request.getParameter("lastName");
-		String password = request.getParameter("password");
-		
-		System.out.println(firstName);
-		System.out.println(lastName);
-		System.out.println(password);
-		
-		request.getRequestDispatcher("/html/userProfile.jsp").forward(request, response);
-		
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		HttpSession session = request.getSession(false);
 		
-		
-		System.out.println(session.getAttribute("loginEmail"));
-		System.out.println(session.getAttribute("firstName"));
-		System.out.println(session.getAttribute("lastName"));
-		System.out.println(session.getAttribute("password"));
+		if (session != null) {
 
-		
+			String loginEmail = (String) session.getAttribute("loginEmail");
+			String firstName = request.getParameter("firstName");
+			String lastName = request.getParameter("lastName");
+			String password = request.getParameter("password");
+			int isUpdated = 1;
+			
+			try {
 
-		
-		
-		if (session != null) {	
-		
-			
-			
-	
-		
-		String loginEmail = (String) session.getAttribute("loginEmail");
-		String firstName  = (String) session.getAttribute("firstName");
-		String lastName  = (String) session.getAttribute("lastName");
-		String password  = (String) session.getAttribute("password");
-		
-		System.out.println(loginEmail);
-		System.out.println(firstName);
-		System.out.println(lastName);
-		System.out.println(password);
-		
-			
-		try {
 				UserDAO userDAO = new UserDAO();
-				
+
 				userDAO.updateUserDataInDatabase(loginEmail, firstName, lastName, password);
-									
-				//User userToUpdate;	
-			
-			/*
-				userToUpdate = userDAO.getSpeicifcUserFromDatabaseByEmail((String)session.getAttribute("loginEmail"));
-	
-			
+
+				User userToUpdate;
+
+				userToUpdate = userDAO.getSpeicifcUserFromDatabaseByEmail((String) session.getAttribute("loginEmail"));
+
 				session.setAttribute("firstName", userToUpdate.getFirstName());
 				session.setAttribute("lastName", userToUpdate.getLastName());
-				session.setAttribute("Password", userToUpdate.getPassword());
-					
-				
-			
-			}catch (ClassNotFoundException | SQLException e) {
+				session.setAttribute("password", userToUpdate.getPassword());
+				session.setAttribute("isUpdated", isUpdated);
+
+				request.getRequestDispatcher("/html/userProfile.jsp").forward(request, response);
+
+			} catch (ClassNotFoundException | SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			*/
+
 		}
-		
+
+		/*
+		String loginEmail = (String) session.getAttribute("loginEmail");
+		if (loginEmail == null)
+
+		{
+			request.setAttribute("errorMessage", "Error no user logged in");
+			RequestDispatcher errorDispatcher = request.getRequestDispatcher("/html/error.jsp");
+			errorDispatcher.forward(request, response);
+		}
+		 */
 	}
 
-
-	
-
+}
