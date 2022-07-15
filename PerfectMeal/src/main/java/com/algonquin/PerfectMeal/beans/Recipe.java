a6ac01c47e33d8291badc217514caba9d58873b5
teepidge <taylor.pigeon@gmail.com>
2022-07-14 22:37:55 -0400

    Added getServletContext() calls to the request Dispatcher
    This seems to fix most of our path issues.  Erik pulled changes an
    confirmed.

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/Recipe.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/Recipe.java
@@ -1,67 +1,66 @@
 package com.algonquin.PerfectMeal.beans;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.UUID;
-
 public class Recipe {
 	private int mealID;
 	private String mealName;
 	private String description;
 	private String CookTime;
 	private String MealLink;
-	
-	
-    public Recipe() {
-    }
-    public Recipe(int mealID, String mealName, String description, String CookTime, String MealLink) {
-        this.mealID = mealID;
-        this.mealName = mealName;
-        this.description = description;
-        this.CookTime = CookTime;
-        this.MealLink = MealLink;
-    }
+
+	public Recipe() {
+	}
+
+	public Recipe(int mealID, String mealName, String description, String CookTime, String MealLink) {
+		this.mealID = mealID;
+		this.mealName = mealName;
+		this.description = description;
+		this.CookTime = CookTime;
+		this.MealLink = MealLink;
+	}
+
 	public void setId(int mealID) {
 		this.mealID = mealID;
-		
+
 	}
-    public int getId() {
-        return mealID;
-    }
-    
+
+	public int getId() {
+		return mealID;
+	}
+
 	public void setName(String mealName) {
 		this.mealName = mealName;
-		
+
 	}
-    public String getName() {
-        return mealName;
-    }
-    
+
+	public String getName() {
+		return mealName;
+	}
+
 	public void setDescription(String description) {
 		this.description = description;
-		
+
 	}
-    public String getDescription() {
-        return description;
-    }
-    
+
+	public String getDescription() {
+		return description;
+	}
+
 	public void setCookTime(String CookTime) {
 		this.CookTime = CookTime;
-		
+
 	}
-    public String getCookTime() {
-        return CookTime;
-    }
-    
+
+	public String getCookTime() {
+		return CookTime;
+	}
+
 	public void setMealLink(String MealLink) {
 		this.MealLink = MealLink;
-		
+
 	}
-    public String getMealLink() {
-        return MealLink;
-    }
-	
+
+	public String getMealLink() {
+		return MealLink;
+	}
+
 }
--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/servlets/UserProfileServlet.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/servlets/UserProfileServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,41 +12,44 @@ import javax.servlet.http.HttpSession;
 import com.algonquin.PerfectMeal.beans.User;
 import com.algonquin.PerfectMeal.dao.UserDAO;
 
-
 public class UserProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	public UserProfileServlet() {
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("user profile button pressed");
-		
+
 		HttpSession session = request.getSession(false);
-		
-		if(session.getAttribute("loginEmail") != null) {
-			
+
+		if (session.getAttribute("loginEmail") != null) {
+
 			UserDAO userDAO = new UserDAO();
 			User userToLogin;
-			
+
 			try {
-			
-			userToLogin = userDAO.getSpeicifcUserFromDatabaseByEmail((String)session.getAttribute("loginEmail"));
-	
-			session.setAttribute("userEmail", userToLogin.getEmail());
-			session.setAttribute("firstName", userToLogin.getFirstName());
-			session.setAttribute("lastName", userToLogin.getLastName());
-			session.setAttribute("password", userToLogin.getPassword());
-			
-			request.getRequestDispatcher("/html/userProfile.jsp").forward(request, response);
-			
-			}catch (ClassNotFoundException | SQLException e) {
+
+				userToLogin = userDAO.getSpeicifcUserFromDatabaseByEmail((String) session.getAttribute("loginEmail"));
+
+				session.setAttribute("userEmail", userToLogin.getEmail());
+				session.setAttribute("firstName", userToLogin.getFirstName());
+				session.setAttribute("lastName", userToLogin.getLastName());
+				session.setAttribute("password", userToLogin.getPassword());
+
+				request.getRequestDispatcher("/html/userProfile.jsp").forward(request, response);
+
+			} catch (ClassNotFoundException | SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}			
+			}
 		}
 	}
 }
