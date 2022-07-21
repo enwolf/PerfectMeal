7287daeab54f3bb5f241787a6b62493ba8448187
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-21 17:59:55 -0400

    Updated Class File: HeaderTextFactory.java
    Added method:
    
    	- String getHeaderText(String pageName)
    
    	This method updated header text via switch statement, output
    	working	correctly on all pages.
    
    Added method:
    
    	-  String getHeaderTextObj(String pageName)
    
    	This is the proper factory pattern method, currently prototyping
    	how this might work properly but I have a few ideas.

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/HeaderTextFactory.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/HeaderTextFactory.java
@@ -6,7 +6,10 @@ public class HeaderTextFactory {
 
 	}
 
-	public static String getHeaderText(String pageName) {
+	// base header and maybe userHeader or loginHeader object that holds current users login value?
+	// Dispatch each as needed as a way to more properly implement this pattern.
+
+	public static String getHeaderTextObj(String pageName) {
 
 		if (pageName.equals("index.jsp")) {
 			return "Welcome!";
@@ -17,4 +20,51 @@ public class HeaderTextFactory {
 		return null;
 	}
 
+	public static String getHeaderText(String pageName) {
+
+		String headerText = "";
+		switch (pageName) {
+
+		case "index.jsp":
+			headerText = "Welcome!";
+			break;
+		case "recipe.jsp":
+			headerText = "Meal Planning";
+			break;
+		case "registerUserForm.jsp":
+			headerText = "Please Registar";
+			break;
+		case "login.jsp":
+			headerText = "Logged In";
+			break;
+		case "userProfile.jsp":
+			headerText = "Current User: ";
+			break;
+		case "logout.jsp":
+			headerText = "Good Bye";
+			break;
+		case "deleteUser.jsp":
+			headerText = "User Deleted";
+			break;
+		case "userAuthentication.jsp":
+			headerText = "Validate User";
+			break;
+		case "forgot-password.jsp":
+			headerText = "Enter Email";
+			break;
+		case "forgot-password-code-validation.jsp":
+			headerText = "Enter Recovery Code";
+			break;
+		case "update-user-password.jsp":
+			headerText = "Confrim Password";
+			break;
+		case "debugPage.jsp":
+		case "errorPage.jsp":
+			headerText = "Error: Somthing went wrong";
+			break;
+		default:
+			// code block
+		}
+		return headerText;
+	}
 }
