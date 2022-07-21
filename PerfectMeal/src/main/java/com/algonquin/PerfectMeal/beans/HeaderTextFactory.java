8c3cd5437446cf848937cfda04732c2bea066cd0
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-21 17:02:49 -0400

    Updated Class File: HeaderTextFactory.java
    Added default constructor.
    
    Added new method:
    
    	- public static String getHeaderText(pageName);
    
    Added test logic to factory class to see if my logic is working as
    expected for index.jsp page and recipe.jsp page.
    
    After testing currently working as expected. Will begin adding code to
    handle the rest of the.jsp pages.

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/HeaderTextFactory.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/HeaderTextFactory.java
@@ -2,4 +2,19 @@ package com.algonquin.PerfectMeal.beans;
 
 public class HeaderTextFactory {
 
+	public HeaderTextFactory() {
+
+	}
+
+	public static String getHeaderText(String pageName) {
+
+		if (pageName.equals("index.jsp")) {
+			return "Welcome!";
+		}
+		if (pageName.equals("recipe.jsp")) {
+			return "Recipe!";
+		}
+		return null;
+	}
+
 }
