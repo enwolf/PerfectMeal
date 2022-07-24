db87b0bd3fac5931aa9d330ee27fdd22b8543a4a
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-23 21:12:23 -0400

    Updated Class File: RegistarUserProxyDAO.java
    Proxy class now implements interface: Subject.java
    
    Added Interface Methods:
    
    	- registerObserver(Observer o)
    
    	- removeObserver(Observer o)
    
    	- notifyObservers()
    
    Implemented logic for interface methods.
    
     	- registerObserver() add observer to array list
    
     	- removeObserver() removes observer from array list
    
     	- notifyObservers() loops through observer list and
     	  calls the dispachnewUserVarifaactionEmail() method
    
    Added objects of type UserDAO, User and ArrayList to proxy class.
    
    Updated createUser() method signature to void from int and also added
    call to	notifyObservers() after user is created.

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/dao/RegisterUserProxyDAO.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/dao/RegisterUserProxyDAO.java
@@ -1,15 +1,48 @@
 package com.algonquin.PerfectMeal.dao;
 
+import java.util.ArrayList;
+
+import com.algonquin.PerfectMeal.beans.Observer;
+import com.algonquin.PerfectMeal.beans.Subject;
 import com.algonquin.PerfectMeal.beans.User;
 
-public class RegisterUserProxyDAO {
-
+public class RegisterUserProxyDAO implements Subject {
+	// Original Object we are acting as proxy for.
 	private UserDAO userDAO = new UserDAO();
+	// list of observers
+	private ArrayList<Observer> observers;
+	// local user object.
+	private User userProxy;
 
-	public int createUser(User user) throws ClassNotFoundException {
+	public RegisterUserProxyDAO() {
+		this.userProxy = new User();
+		this.observers = new ArrayList<Observer>();
+	}
 
-		return userDAO.createUser(user);
+	public void createUser(User user) throws ClassNotFoundException {
+		userProxy = user;
+		userDAO.createUser(userProxy);
+		notifyObservers();
+	}
 
+	@Override
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		int i = observers.indexOf(o);
+		if (i >= 0) {
+			observers.remove(i);
+		}
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (Observer observer : observers) {
+			observer.dispachNewUserVarificationEmail(userProxy);
+		}
 	}
 
 }
