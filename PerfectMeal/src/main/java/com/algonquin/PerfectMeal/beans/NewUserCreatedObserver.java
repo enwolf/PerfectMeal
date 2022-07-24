02789e3a6dafe4ed7eccca7117056bb5e6327708
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-23 21:58:43 -0400

    Updated Class File: NewUserCreatedObserver.java
    Added final String fromAddress and set value to: validate@fake.fake
    
    Added Subject registerUserProxyDAO object.
    
    Added default constructor, following the observer pattern we used in
    assignment 5.
    
    Added Methods:
    
    	- dispachNewUserVarificationEmail(User user)
    
    	  This method uses data from the user object to format into message
    	  to be sent as email for new user to validate their account.
    
    	- generatetEmail(String from, String to, String message, String code)
    
    	  This method accept Strings args to use in formating email, currently
    	  output is displayed in a sysout message to the console as we have not
    	  figured out how to send actual email.

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/NewUserCreatedObserver.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/NewUserCreatedObserver.java
@@ -1,5 +1,29 @@
 package com.algonquin.PerfectMeal.beans;
 
-public class NewUserCreatedObserver {
+public class NewUserCreatedObserver implements Observer {
+
+	private final String fromAddress = "validate@fake.fake";
+	private Subject registerUserProxyDAO;
+
+	public NewUserCreatedObserver(Subject registerUserProxyDAO) {
+		this.registerUserProxyDAO = registerUserProxyDAO;
+		registerUserProxyDAO.registerObserver(this);
+	}
+
+	@Override
+	public void dispachNewUserVarificationEmail(User user) {
+
+		String emailHeaderFrom = "From: " + fromAddress + "\n";
+		String emailHeaderTo = "To: " + user.getEmail() + "\n\n";
+		String message = "Hello " + user.getFirstName() + " " + user.getLastName() + " welcome to Perfect Meal! \n";
+		String code = "Please use this code to verify your account: " + user.getId();
+
+		generatetEmail(emailHeaderFrom, emailHeaderTo, message, code);
+
+	}
+
+	public void generatetEmail(String from, String to, String message, String code) {
+		System.out.println(from + to + message + code);
+	}
 
 }
