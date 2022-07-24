33d9d1efa4cdfc97364be099335ddd1022bcd4e4
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-23 20:38:00 -0400

    Updated Interface File: Subject.java
    Added Subject Interface methods:
    
    	- registerObserver(Observer o)
    
    	- removeObserver(Observer o)
    
    	- notifyObservers()

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/Subject.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/beans/Subject.java
@@ -2,4 +2,9 @@ package com.algonquin.PerfectMeal.beans;
 
 public interface Subject {
 
+	public void registerObserver(Observer o);
+
+	public void removeObserver(Observer o);
+
+	public void notifyObservers();
 }
