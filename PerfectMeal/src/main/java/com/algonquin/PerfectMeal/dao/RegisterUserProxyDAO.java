33c2f80e12db9e0a7d67af1b0fd358e1b11ab340
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-23 20:32:56 -0400

    Updated Class File: RegisterUserProxyDAO.java
    Added UserDAO object to use in proxy methods.
    
    Added method required by proxy class:
    
     - public int createUser(User user)

--- PerfectMeal/src/main/java/com/algonquin/PerfectMeal/dao/RegisterUserProxyDAO.java
+++ PerfectMeal/src/main/java/com/algonquin/PerfectMeal/dao/RegisterUserProxyDAO.java
@@ -1,5 +1,15 @@
 package com.algonquin.PerfectMeal.dao;
 
+import com.algonquin.PerfectMeal.beans.User;
+
 public class RegisterUserProxyDAO {
+
+	private UserDAO userDAO = new UserDAO();
+
+	public int createUser(User user) throws ClassNotFoundException {
+
+		return userDAO.createUser(user);
+
+	}
 
 }
